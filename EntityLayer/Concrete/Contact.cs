0e26759d35622a0c7517c170e096b1e3e0dd7956
omersaldiran <osaldiran@gmail.com>
2022-07-05 22:43:22 +0300

    Entityler eklendi

---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,9 @@
+public class Contact{
+    public int ContactID { get; set; }
+    public string ContactUserName { get; set; }
+    public string ContactMail { get; set; }
+    public string ContactSubject { get; set; }
+    public string ContactMessage { get; set; }
+    public DateTime ContactDate { get; set; }
+    public bool ContactStatus { get; set; }
+}
